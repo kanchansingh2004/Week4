400b052d06f2310a6d3ccfa79c495035643630f2
kanchansingh2004 <kannursingh2004@gmail.com>
2025-02-16 15:51:36 +0530

    [Kanchan] Add. Committing Day5 Assignment problem on advance JUnit Testing

---
+++ JunitTesting/src/main/java/com/advancejunittesting/TemperatureConverter.java
@@ -0,0 +1,12 @@
+package com.advancejunittesting;
+
+public class TemperatureConverter {
+    // converts celsius to fahrenheit
+    public double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+    // converts Fahrenheit to celsius
+    public double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5/9;
+    }
+}
