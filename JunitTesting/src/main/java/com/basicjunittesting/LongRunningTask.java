fe198f142ff408b8cd6479362b0791e19a765ef8
kanchansingh2004 <kannursingh2004@gmail.com>
2025-02-16 15:17:38 +0530

    [Kanchan] Add. Committing Day5 Assignment problem Performance Testing using @Timeout

---
+++ JunitTesting/src/main/java/com/basicjunittesting/LongRunningTask.java
@@ -0,0 +1,8 @@
+package com.basicjunittesting;
+
+public class LongRunningTask {
+    public String longRunningTask() throws InterruptedException {
+        Thread.sleep(2000);
+        return "Task Completed";
+    }
+}
